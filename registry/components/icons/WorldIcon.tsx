786fabeecdbab06247a5810cea2bf9511d47bb9e
Denis Hnidenko <denishnidenko@sudolabs.io>
2025-04-24 15:15:04 +0200

    fix: Added icon dependencies for sidebar component

---
+++ registry/components/icons/WorldIcon.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from 'react'
+
+export const WorldIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+    <path d="M3.6 9h16.8" />
+    <path d="M3.6 15h16.8" />
+    <path d="M11.5 3a17 17 0 0 0 0 18" />
+    <path d="M12.5 3a17 17 0 0 1 0 18" />
+  </svg>
+)
